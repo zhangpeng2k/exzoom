fd03309e753da3523d2b4930024ab0400de80896
xxx <xxx_828@163.com>
2018-03-31 22:39:51 +0800

    js改用共享数据；调整css，使之替代一些js里写的固定样式

--- src/ex_zoom.js
+++ src/ex_zoom.js
@@ -1,137 +1,124 @@
 ;(function ($) {
+    var g = {};//全局变量
     $.fn.ex_zoom = function (options) {
         var opts = $.extend({},
             $.fn.ex_zoom.defaults, options);
 
-        var box_ele = $(opts.box_ele),
-            nav_ele = $(opts.nav_ele),
-            nav_current_img_class = opts.nav_current_img_class,
-            box_width = opts.box_width,
-            box_height = opts.box_height,
-            nav_width = opts.nav_width,
-            nav_height = opts.nav_height,
-            nav_border = opts.nav_border,
-            nav_item_margin = opts.nav_item_margin,
-            nav_item_num = opts.nav_item_num,
-            // _last, _next,
-            images = box_ele.find("img"),
-            img_num = images.length,
+        g.box_ele = $(opts.box_ele);
+        g.nav_ele = $(opts.nav_ele);
+        g.nav_current_img_class = opts.nav_current_img_class;
+        g.box_width = opts.box_width;
+        g.box_height = opts.box_height;
+        g.nav_width = opts.nav_width;
+        g.nav_height = opts.nav_height;
+        g.nav_border = opts.nav_border;
+        g.nav_item_margin = opts.nav_item_margin;
+        g.nav_item_num = opts.nav_item_num;
+        g.images = g.box_ele.find("img");
+        g.img_num = g.images.length;
+        g.img_index = 0;
+        g.img_arr = [];
+        g.next_btn = $(opts.next_btn);//缩略图导航下一张按钮
+        g.prev_btn = $(opts.prev_btn);//缩略图导航上一张按钮
+        g.move_index = 0;//缩略图导航索引
 
-            img_index = 0,
-            img_arr = [],
-            next_btn = $(opts.next_btn),//缩略图导航下一张按钮
-            prev_btn = $(opts.prev_btn),//缩略图导航上一张按钮
-            move_index = 0;//缩略图导航索引
+        g.nav_ele.append("<p style='position:absolute;left:0;top:0;margin: 0'></p>");
+        g.nav_ele_inner = g.nav_ele.find("p");
 
-        nav_ele.append("<p style='position:absolute;left:0;top:0;margin: 0'></p>");
-        var nav_ele_inner = nav_ele.find("p");
-
-        for (var i = 0; i < img_num; i++) {
-            img_arr[i] = [];//图片属性数组
-            img_arr[i][0] = images.eq(i).attr("src");
-            img_arr[i][1] = images.eq(i).attr("width");
-            img_arr[i][2] = images.eq(i).attr("height");
-            var img_scale = img_arr[i][1] / img_arr[i][2];
+        for (var i = 0; i < g.img_num; i++) {
+            g.img_arr[i] = [];//图片属性数组
+            g.img_arr[i][0] = g.images.eq(i).attr("src");
+            g.img_arr[i][1] = g.images.eq(i).attr("width");
+            g.img_arr[i][2] = g.images.eq(i).attr("height");
+            var img_scale = g.img_arr[i][1] / g.img_arr[i][2];
             if (img_scale === 1) {
-                img_arr[i][3] = box_width;//width
-                img_arr[i][4] = box_height;//height
-                img_arr[i][5] = 0;//top
-                img_arr[i][6] = 0;//left
-                img_arr[i][7] = box_width / 2;
-                img_arr[i][8] = box_width * 2;//width
-                img_arr[i][9] = box_height * 2;//height
-                nav_ele_inner.append("<span><img src='" + images.eq(i).attr("src") + "' width='" + nav_width +
-                    "' height='" + nav_height + "' /></span>");
+                g.img_arr[i][3] = g.box_width;//width
+                g.img_arr[i][4] = g.box_height;//height
+                g.img_arr[i][5] = 0;//top
+                g.img_arr[i][6] = 0;//left
+                g.img_arr[i][7] = g.box_width / 2;
+                g.img_arr[i][8] = g.box_width * 2;//width
+                g.img_arr[i][9] = g.box_height * 2;//height
+                g.nav_ele_inner.append("<span><img src='" + g.images.eq(i).attr("src") + "' width='" + g.nav_width +
+                    "' height='" + g.nav_height + "' /></span>");
             } else if (img_scale > 1) {
-                img_arr[i][3] = box_width;//width
-                img_arr[i][4] = box_width / img_scale;
-                img_arr[i][5] = (box_height - img_arr[i][4]) / 2;
-                img_arr[i][6] = 0;//left
-                img_arr[i][7] = img_arr[i][4] / 2;
-                img_arr[i][8] = box_height * 2 * img_scale;//width
-                img_arr[i][9] = box_height * 2;//height
-                nav_ele_inner.append("<span><img src='" + images.eq(i).attr("src") + "' width='" + nav_width +
-                    "' style='top:" + (nav_height - (nav_width / img_scale)) / 2 + "px;' /></span>");
+                g.img_arr[i][3] = g.box_width;//width
+                g.img_arr[i][4] = g.box_width / img_scale;
+                g.img_arr[i][5] = (g.box_height - g.img_arr[i][4]) / 2;
+                g.img_arr[i][6] = 0;//left
+                g.img_arr[i][7] = g.img_arr[i][4] / 2;
+                g.img_arr[i][8] = g.box_height * 2 * img_scale;//width
+                g.img_arr[i][9] = g.box_height * 2;//height
+                g.nav_ele_inner.append("<span><img src='" + g.images.eq(i).attr("src") + "' width='" + g.nav_width +
+                    "' style='top:" + (g.nav_height - (g.nav_width / img_scale)) / 2 + "px;' /></span>");
             } else if (img_scale < 1) {
-                img_arr[i][3] = box_height * img_scale;//width
-                img_arr[i][4] = box_height;//height
-                img_arr[i][5] = 0;//top
-                img_arr[i][6] = (box_width - img_arr[i][3]) / 2;
-                img_arr[i][7] = img_arr[i][3] / 2;
-                img_arr[i][8] = box_width * 2;//width
-                img_arr[i][9] = box_width * 2 / img_scale;
-                nav_ele_inner.append("<span><img src='" + images.eq(i).attr("src") + "' height='" + nav_height +
-                    "' style='left:" + (nav_width - (nav_height * img_scale)) / 2 + "px;' /></span>");
+                g.img_arr[i][3] = g.box_height * img_scale;//width
+                g.img_arr[i][4] = g.box_height;//height
+                g.img_arr[i][5] = 0;//top
+                g.img_arr[i][6] = (g.box_width - g.img_arr[i][3]) / 2;
+                g.img_arr[i][7] = g.img_arr[i][3] / 2;
+                g.img_arr[i][8] = g.box_width * 2;//width
+                g.img_arr[i][9] = g.box_width * 2 / img_scale;
+                g.nav_ele_inner.append("<span><img src='" + g.images.eq(i).attr("src") + "' height='" + g.nav_height +
+                    "' style='left:" + (g.nav_width - (g.nav_height * img_scale)) / 2 + "px;' /></span>");
             }
         }
-        images.css("display", "none").addClass("ex_zoom_hidden_img");
-        // images.remove();
+        g.images.remove();
 
-        nav_ele.append("<div style='clear:both;width:100%;'></div>");
-        var nav_ele_span = nav_ele.find("span");
-        var nav_ele_img = nav_ele.find("img");
-        var _border = nav_border * 2 + nav_height;
-        var nav_ele_width = (_border + nav_item_margin) * nav_item_num;
-        var nav_ele_inner_width = (_border + nav_item_margin) * img_num;
+        // nav_ele.append("<div style='clear:both;width:100%;'></div>");
+        g.nav_ele_span = g.nav_ele.find("span");
+        g.nav_height_with_border = g.nav_border * 2 + g.nav_height;
+        g.nav_ele_width = (g.nav_height_with_border + g.nav_item_margin) * g.nav_item_num;
+        g.nav_ele_inner_width = (g.nav_height_with_border + g.nav_item_margin) * g.img_num;
 
-        nav_ele_span.eq(img_index).addClass(nav_current_img_class);
-        nav_ele.css({
-            "overflow": "hidden",
-            "height": _border + "px",
-            "width": nav_ele_width + "px",
-            "position": "relative",
+        g.nav_ele_span.eq(g.img_index).addClass(g.nav_current_img_class);
+        g.nav_ele.css({
+            "height": g.nav_height_with_border + "px",
+            "width": g.nav_ele_width + "px",
         });
-        nav_ele_inner.css({
-            "width": nav_ele_inner_width + "px"
+        g.nav_ele_inner.css({
+            "width": g.nav_ele_inner_width + "px"
         });
-        nav_ele_span.css({
-            "float": "left",
-            "margin-left": nav_item_margin + "px",
-            "width": nav_width + "px",
-            "height": nav_height + "px",
-            "overflow": "hidden",
-            "position": "relative"
-        });
-        nav_ele_img.css({
-            "max-width": "100%",
-            "max-height": "100%",
-            "position": "relative"
+        g.nav_ele_span.css({
+            "margin-left": g.nav_item_margin + "px",
+            "width": g.nav_width + "px",
+            "height": g.nav_height + "px",
         });
 
         //大图和预览区域部分
-        box_ele.append("<div style='position:relative;'><b style='display:block;'>" +
-            "<img style='display:block;' src='' /></b>" +
+        g.box_ele.append("<div style='position:relative;'><b style='display:block;'>" +
+            "<img style='display:block;' src='' class='ex_zoom_main_img'/></b>" +
             "<span style='position:absolute;left:0;top:0;display:none;z-index:5;'></span>" +
             "</div><p style='position:absolute;overflow:hidden;top:0;display:none;'>" +
             "<img style='max-width:none;max-height:none;position:relative;left:0;top:0;' src='' /></p>");
 
-        var zoom_zone = box_ele.find("span");
-        var box_ele_img = box_ele.find("b img");
-        var box_ele_img_outter = box_ele.find("div");
-        var preview_box = box_ele.find("p");
-        var preview_box_img = box_ele.find("p img");
+        g.zoom_zone = g.box_ele.find("span");
+        g.box_ele_img = g.box_ele.find("b img");
+        g.box_ele_img_outer = g.box_ele.find("div");
+        g.preview_box = g.box_ele.find("p");
+        g.preview_box_img = g.box_ele.find("p img");
 
-        box_ele.css({
-            "width": box_width + "px",
-            "height": box_height + "px",
+        g.box_ele.css({
+            "width": g.box_width + "px",
+            "height": g.box_height + "px",
             "position": "relative"
         });
 
-        preview_box.css({
-            "width": box_width + "px",
-            "height": box_height + "px",
-            "left": box_width + 10 + "px"
+        g.preview_box.css({
+            "width": g.box_width + "px",
+            "height": g.box_height + "px",
+            "left": g.box_width + 10 + "px"
         });
 
         preview_img();
 
         //在大图区域移动
-        box_ele_img_outter.on("mousemove", function (e) {
-            var width_limit = zoom_zone.width() / 2,
-                max_X = box_ele_img_outter.width() - width_limit,
-                max_Y = box_ele_img_outter.height() - width_limit,
-                current_X = e.pageX - box_ele_img_outter.offset().left,
-                current_Y = e.pageY - box_ele_img_outter.offset().top,
+        g.box_ele_img_outer.on("mousemove", function (e) {
+            var width_limit = g.zoom_zone.width() / 2,
+                max_X = g.box_ele_img_outer.width() - width_limit,
+                max_Y = g.box_ele_img_outer.height() - width_limit,
+                current_X = e.pageX - g.box_ele_img_outer.offset().left,
+                current_Y = e.pageY - g.box_ele_img_outer.offset().top,
                 move_X = current_X - width_limit,
                 move_Y = current_Y - width_limit;
 
@@ -147,118 +134,128 @@
             if (current_Y >= max_Y) {
                 move_Y = max_Y - width_limit;
             }
-            zoom_zone.css({"left": move_X + "px", "top": move_Y + "px"});
+            g.zoom_zone.css({"left": move_X + "px", "top": move_Y + "px"});
 
-            preview_box_img.css({
-                "left": -move_X * preview_box.width() / zoom_zone.width() + "px",
-                "top": -move_Y * preview_box.width() / zoom_zone.width() + "px"
+            g.preview_box_img.css({
+                "left": -move_X * g.preview_box.width() / g.zoom_zone.width() + "px",
+                "top": -move_Y * g.preview_box.width() / g.zoom_zone.width() + "px"
             });
         });
 
         //进入进入大图区域
-        box_ele_img_outter.on("mouseenter", function () {
-            zoom_zone.css("display", "block");
-            preview_box.css("display", "block");
+        g.box_ele_img_outer.on("mouseenter", function () {
+            g.zoom_zone.css("display", "block");
+            g.preview_box.css("display", "block");
         });
 
         //离开大图区域
-        box_ele_img_outter.on("mouseleave", function () {
-            zoom_zone.css("display", "none");
-            preview_box.css("display", "none");
+        g.box_ele_img_outer.on("mouseleave", function () {
+            g.zoom_zone.css("display", "none");
+            g.preview_box.css("display", "none");
         });
 
         //缩略图导航
-        if (img_num <= nav_item_num) {
-            nav_ele_inner.css("left", (nav_item_num - img_num) * _border / 2 + "px");
+        if (g.img_num <= g.nav_item_num) {
+            g.nav_ele_inner.css("left", (g.nav_item_num - g.img_num) * g.nav_height_with_border / 2 + "px");
         }
-        next_btn.on("click", function () {
+        g.next_btn.on("click", function () {
             move_right();
         });
-        prev_btn.on("click", function () {
+        g.prev_btn.on("click", function () {
             move_left();
         });
 
-        nav_ele_span.hover(function () {
-            img_index = $(this).index();
-            nav_ele_span.eq(img_index).addClass(nav_current_img_class).siblings().removeClass(nav_current_img_class);
+        g.nav_ele_span.hover(function () {
+            g.img_index = $(this).index();
+            g.nav_ele_span.eq(g.img_index).addClass(g.nav_current_img_class).siblings().removeClass(g.nav_current_img_class);
             preview_img();
         });
 
-        function move_right() {
-            if (img_num <= nav_item_num) {
-                img_index++;
-                if (img_index >= img_num) {
-                    img_index = img_num - 1;
-                }
-                nav_ele_span.eq(img_index).addClass(nav_current_img_class).siblings().removeClass(nav_current_img_class);
-                preview_img();
-            } else {
-                move_index++;
-                var max_num = img_num - nav_item_num;
-                if (move_index >= max_num) {
-                    move_index = max_num;
-                }
-                if (img_index <= move_index) {
-                    img_index = move_index;
-                }
-                nav_ele_move();
-            }
-        }
-
-        function move_left() {
-            if (img_num <= nav_item_num) {
-                img_index--;
-                if (img_index <= 0) {
-                    img_index = 0;
-                }
-                nav_ele_span.eq(img_index).addClass(nav_current_img_class).siblings().removeClass(nav_current_img_class);
-                preview_img();
-            } else {
-                move_index--;
-                if (move_index <= 0) {
-                    move_index = 0;
-                }
-                if (img_index >= move_index + nav_item_num) {
-                    img_index = move_index + nav_item_num - 1;
-                }
-                nav_ele_move();
-            }
-
-        }
-
-        function preview_img() {
-            preview_box_img.attr("src", img_arr[img_index][0]);
-            box_ele_img.attr("src", img_arr[img_index][0]);
-            box_ele_img.css({
-                "width": img_arr[img_index][3] + "px",
-                "height": img_arr[img_index][4] + "px"
-            });
-            box_ele_img_outter.css({
-                "width": img_arr[img_index][3] + "px",
-                "height": img_arr[img_index][4] + "px",
-                "top": img_arr[img_index][5] + "px",
-                "left": img_arr[img_index][6] + "px",
-                "position": "relative"
-            });
-            zoom_zone.css({
-                "width": img_arr[img_index][7] + "px",
-                "height": img_arr[img_index][7] + "px"
-            });
-            preview_box_img.css({
-                "width": img_arr[img_index][8] + "px",
-                "height": img_arr[img_index][9] + "px"
-            });
-        }
-
-
-        function nav_ele_move() {
-            nav_ele_inner.css({left: -move_index * (_border + nav_item_margin) + "px"});
-            nav_ele_span.eq(img_index).addClass(nav_current_img_class).siblings().removeClass(nav_current_img_class);
-            preview_img();
-        }
-
         return this;
     };
+
+    /**
+     * 导航向右
+     */
+    function move_right() {
+        if (g.img_num <= g.nav_item_num) {
+            g.img_index++;
+            if (g.img_index >= g.img_num) {
+                g.img_index = g.img_num - 1;
+            }
+            g.nav_ele_span.eq(g.img_index).addClass(g.nav_current_img_class).siblings().removeClass(g.nav_current_img_class);
+            preview_img();
+        } else {
+            var max_num = g.img_num - g.nav_item_num;
+            g.move_index++;
+            if (g.move_index >= max_num) {
+                g.move_index = max_num;
+            }
+            if (g.img_index <= g.move_index) {
+                g.img_index = g.move_index;
+            }
+            nav_ele_move();
+        }
+    }
+
+    /**
+     * 导航向左
+     */
+    function move_left() {
+        if (g.img_num <= g.nav_item_num) {
+            g.img_index--;
+            if (g.img_index <= 0) {
+                g.img_index = 0;
+            }
+            g.nav_ele_span.eq(g.img_index).addClass(g.nav_current_img_class).siblings().removeClass(g.nav_current_img_class);
+            preview_img();
+        } else {
+            g.move_index--;
+            if (g.move_index <= 0) {
+                g.move_index = 0;
+            }
+            if (g.img_index >= g.move_index + g.nav_item_num) {
+                g.img_index = g.move_index + g.nav_item_num - 1;
+            }
+            nav_ele_move();
+        }
+    }
+
+    /**
+     * 移动导航图片
+     */
+    function nav_ele_move() {
+        g.nav_ele_inner.css({left: -g.move_index * (g.nav_height_with_border + g.nav_item_margin) + "px"});
+        g.nav_ele_span.eq(g.img_index).addClass(g.nav_current_img_class).siblings().removeClass(g.nav_current_img_class);
+        preview_img();
+    }
+
+    /**
+     * 预览图片
+     */
+    function preview_img() {
+        g.preview_box_img.attr("src", g.img_arr[g.img_index][0]);
+        g.box_ele_img.attr("src", g.img_arr[g.img_index][0]);
+        g.box_ele_img.css({
+            "width": g.img_arr[g.img_index][3] + "px",
+            "height": g.img_arr[g.img_index][4] + "px"
+        });
+        g.box_ele_img_outer.css({
+            "width": g.img_arr[g.img_index][3] + "px",
+            "height": g.img_arr[g.img_index][4] + "px",
+            "top": g.img_arr[g.img_index][5] + "px",
+            "left": g.img_arr[g.img_index][6] + "px",
+            "position": "relative"
+        });
+        g.zoom_zone.css({
+            "width": g.img_arr[g.img_index][7] + "px",
+            "height": g.img_arr[g.img_index][7] + "px"
+        });
+        g.preview_box_img.css({
+            "width": g.img_arr[g.img_index][8] + "px",
+            "height": g.img_arr[g.img_index][9] + "px"
+        });
+    }
 
     // 插件的defaults
     $.fn.ex_zoom.defaults = {
